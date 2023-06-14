becda9d7fc15cb5a665cd0366dd1ed876b9d583b
Björn Benouarets <70456959+bbenouarets@users.noreply.github.com>
2023-06-14 11:10:32 +0200

    Fix name of pypi package

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyresty",
+    name="turborest",
     version="0.1.0",
     author="ByteSentinel.io",
     author_email="dev@bytesentinel.io",
     description="A simple REST client for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/bytesentinel-io/pyresty",
+    url="https://github.com/bytesentinel-io/turborest",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
